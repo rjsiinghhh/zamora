abcabeac64ec67dea56348086ae4933751677a3e
rjsiinghhh <rjsiinghhh@gmail.com>
2021-01-11 19:53:49 -0800

    18

--- client/src/finance/components/ExpenseTable.js
+++ client/src/finance/components/ExpenseTable.js
@@ -9,27 +9,26 @@ const ExpenseTable = () => {
     <Table striped bordered hover variant="dark">
   <thead>
     <tr>
-      <th>#</th>
-      <th>First Name</th>
-      <th>Last Name</th>
-      <th>Username</th>
+      <th>Date</th>
+      <th>Description</th>
+      <th>Amount</th>
     </tr>
   </thead>
   <tbody>
     <tr>
-      <td>1</td>
+  
       <td>Mark</td>
       <td>Otto</td>
       <td>@mdo</td>
     </tr>
     <tr>
-      <td>2</td>
+     
       <td>Jacob</td>
       <td>Thornton</td>
       <td>@fat</td>
     </tr>
     <tr>
-      <td>3</td>
+     
       <td colSpan="2">Larry the Bird</td>
       <td>@twitter</td>
     </tr>
